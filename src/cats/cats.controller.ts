cdbd3227e3123cf8d0fef962bb01275b03cb8219
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2025-02-15 23:40:13 +0500

    created basic service

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res} from '@nestjs/common';
+import { CreateCatDto } from './utils/CreateCat.dto';
+import { Observable, of } from 'rxjs';
 
 @Controller('cats')
 export class CatsController {
     @Get()
-    findAll() {
-        return 'This action returns all cats';
+    findAll() :Observable<string> {
+        return of('This action returns all cats');
+    }
+
+    @Post()
+    create(@Body() createCatDto: CreateCatDto,@Res() response) {
+        response.status(201).send();
+    }
+    @Get(":id")
+    findOne(@Param('id') id: string) {
+        return `This action returns a ${id} cat`;
     }
 }
---
+++ src/cats/utils/CreateCat.dto.ts
@@ -0,0 +1,7 @@
+// import { IsString, IsInt } from 'class-validator';
+
+export class CreateCatDto {
+    name: string;
+    age: number;
+    breed: string;
+}
